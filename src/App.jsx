a2e6290c0c42b2eac398a401c4c2ae946125f7ae
김채영 <cccodud@gmail.com>
2023-05-22 20:35:10 +0900

    init: 초기세팅2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ExRouting from './Routing/ExRouting';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="app-container">
+                <ExRouting />
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Routing/DATA.js
@@ -0,0 +1,42 @@
+export const DATA = [
+    {
+        name: 'elice',
+        addr: 'seoul',
+    },
+    {
+        name: 'react',
+        addr: 'gyeongi',
+    },
+    {
+        name: 'dev',
+        addr: 'incheon',
+    },
+    {
+        name: 'webdev',
+        addr: 'daegu',
+    },
+    {
+        name: 'front',
+        addr: 'daejeon',
+    },
+    {
+        name: 'back',
+        addr: 'sejong',
+    },
+    {
+        name: 'redux',
+        addr: 'ulsan',
+    },
+    {
+        name: 'router',
+        addr: 'gwangju',
+    },
+    {
+        name: 'jest',
+        addr: 'busan',
+    },
+    {
+        name: 'dom',
+        addr: 'jeju',
+    },
+];
